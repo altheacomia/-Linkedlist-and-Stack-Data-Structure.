e05dfe4f612181e009a2b8e687c16ffffdf4feae
altheacomia <altheacomia98@gmail.com>
2025-11-16 20:08:03 +0800

    Node

---
+++ Node.java
@@ -0,0 +1,11 @@
+// File: Node.java
+public class Node {
+    int data;     // Value stored in the node
+    Node next;    // Pointer to the next node
+
+    // Constructor
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
